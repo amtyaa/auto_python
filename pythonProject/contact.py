724694c178189287c9ff571b3d151e9bd239dad1
Таня <t9051980712@gmail.com>
2024-06-18 23:04:38 +0300

    добавлен вспомогательный класс

---
+++ pythonProject/contact.py
@@ -0,0 +1,32 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company,
+                  address, home, mobile, work, fax, email, email2, email3,
+                  homepage, ayear2, address2, phone2, notes, byear1,
+                  bday1, bmonth1, aday2,
+                  amonth2):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.ayear2 = ayear2
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+        self.byear1 = byear1
+        self.bday1 = bday1
+        self.bmonth1 = bmonth1
+        self.aday2 = aday2
+        self.amonth2 = amonth2
