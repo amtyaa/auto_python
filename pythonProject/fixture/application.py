69b13d07c735c6677fc3091ebb39f34bf91f8ed0
Таня <t9051980712@gmail.com>
2024-07-06 21:09:24 +0300

    Создан файл сессии отдельно. добавлен импорт и нужные ссылки

--- pythonProject/fixture/application.py
+++ pythonProject/fixture/application.py
@@ -1,18 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from fixture.session import SessionHelper
+
 
 class Application:
     def __init__(self):
         self.driver = webdriver.Firefox()
         self.vars = {}
+        self.session = SessionHelper(self)
 
     def destroy(self):
         self.driver.quit()
-
-    def logout(self):
-        # Logout
-        self.driver.find_element(By.LINK_TEXT, "Logout").click()
 
     def Creat_group(self, group):
         # Creat group
@@ -27,15 +26,6 @@ class Application:
         self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer_group)
         self.driver.find_element(By.NAME, "submit").click()
 
-
-    def Login(self, username, password):
-        # Login
-        self.driver.get("http://localhost/addressbook/")
-        self.driver.find_element(By.NAME, "user").click()
-        self.driver.find_element(By.NAME, "user").send_keys(username)
-        self.driver.find_element(By.NAME, "pass").click()
-        self.driver.find_element(By.NAME, "pass").send_keys(password)
-        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
 
     def Add_contact(self, contact):
         # Add new contact
---
+++ pythonProject/fixture/session.py
@@ -0,0 +1,16 @@
+class SessionHelper():
+    def __init__(self, app):
+        self.app = app
+
+    def logout(self):
+        # Logout
+        self.app.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def Login(self, username, password):
+        # Login
+        self.app.driver.get("http://localhost/addressbook/")
+        self.app.driver.find_element(By.NAME, "user").click()
+        self.app.driver.find_element(By.NAME, "user").send_keys(username)
+        self.app.driver.find_element(By.NAME, "pass").click()
+        self.app.driver.find_element(By.NAME, "pass").send_keys(password)
+        self.app.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
